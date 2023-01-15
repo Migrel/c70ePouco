d6179fa480e1f8cee2471805c9651251c0d11f5f
Migrel <111578877+Migrel@users.noreply.github.com>
2023-01-15 20:52:20 -0300

    Add files via upload

---
+++ console.js
@@ -0,0 +1,12 @@
+var first_name = "Ranbir";
+var last_name = "kapoor";
+var a = 10;
+var b = 5;
+var add = a + b;
+var ranbirKapoor = last_name + first_name;
+function carregou(){
+    console.log(add);
+    console.log(b);
+console.log(a);
+console.log(ranbirKapoor);
+}
